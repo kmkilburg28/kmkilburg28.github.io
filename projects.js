c8b80e23df69b93dfcfe372793cbe4c072918592
Kaden Kilburg <kadenkilburg@kadenk-macbook.student.iastate.edu>
2022-01-24 16:18:15 -0600

    Updated project header links to be stored separately from header text

--- projects.js
+++ projects.js
@@ -4,24 +4,28 @@ const PROJECTS = {
 			"name": "Artificial Intelligence",
 			"projects": [
 				{
-					"name": "<a href='https://github.com/kmkilburg28/MultiAgentSurvival'>Multi-agent Survival Game</a>",
+					"name": "Multi-agent Survival Game",
 					"description": "A term project for Iowa State's graduate course Principles of Artificial Intelligence. This project was inspired by OpenAI's <a href='https://openai.com/blog/neural-mmo/'>NeuralMMO</a> and can be thought of as a beginner-friendly version. Multiple reinforcement learning algorithms have been tested for their performance at training intelligent agents, which are detailed in this <a href='reports/multiagentsurvival.pdf'>report</a>.",
-					"graphic": "graphics/multiagentsurvival.mp4"
+					"graphic": "graphics/multiagentsurvival.mp4",
+					"link": "https://github.com/kmkilburg28/MultiAgentSurvival"
 				},
 				{
-					"name": "<a href='https://github.com/kmkilburg28/NBA_Predictions'>NBA Predictions</a>",
+					"name": "NBA Predictions",
 					"description": "After collecting a database of basketball statistics from 2000 to 2021, machine learning techniques were tested for their classification performance on selecting a winning team given each team's player statistics. For the methods tested, a deep learning approach where each player's season statistics were fed through a recurrent layer performed the best. Further details can be found in this <a href='reports/nba_predictions.pdf'>report</a>.",
-					"graphic": "graphics/nba_predictions.png"
+					"graphic": "graphics/nba_predictions.png",
+					"link": "https://github.com/kmkilburg28/NBA_Predictions"
 				},
 				{
-					"name": "<a href='Checkers/'>Checkers AI</a>",
+					"name": "Checkers AI",
 					"description": "A standard checkers board allowing human play as well as customized AI players. The customization comes in the form of selecting an evaluation heuristic from those provided as well as a depth to search to throughout its alpha-beta pruning.",
-					"graphic": "graphics/checkers.png"
+					"graphic": "graphics/checkers.png",
+					"link": "Checkers/"
 				},
 				{
-					"name": "<a href='KerasClone/'>Keras Clone</a>",
+					"name": "Keras Clone",
 					"description": "A basic implementation of the Keras API. It supports dense hidden layers as well as linear, ReLU, and sigmoid activation functions.",
-					"graphic": "graphics/kerasclone.png"
+					"graphic": "graphics/kerasclone.png",
+					"link": "KerasClone/"
 				},
 				{
 					"name": "Student Major Predictor",
@@ -34,24 +38,28 @@ const PROJECTS = {
 			"name": "Computer Vision",
 			"projects": [
 				{
-					"name": "<a href='IntonationLearner/'>In-Tone Nation</a>",
+					"name": "In-Tone Nation",
 					"description": "An intonation learning application to assist in understanding secondary languages. The application works by placing the user into a call-and-response setting. Here, the user is given an audio clip from a \"model\" speaker, and then, the user is asked to record themselves imitating the speaker. Once the user's voice is recorded, both audio clips' frequencies are placed on a graph for user inspection to determine where major discrepancies may lie between the two clips. A detailed report on the project's implementation and testing results may be found <a href='reports/tonenation.pdf'>here</a>.",
-					"graphic": "graphics/intone_nation.png"
+					"graphic": "graphics/intone_nation.png",
+					"link": "IntonationLearner/"
 				},
 				{
-					"name": "<a href='BackgammonImageProcessing/'>Backgammon Image Processing</a>",
+					"name": "Backgammon Image Processing",
 					"description": "Using OpenCV, multiple scripts were written to perform visual computations on backgammon games. These include modifying piece colors, tracking moving pieces, and scoring the board state.",
-					"graphic": "graphics/backgammon.png"
+					"graphic": "graphics/backgammon.png",
+					"link": "BackgammonImageProcessing/"
 				},
 				{
-					"name": "<a href='AudioProcessing/'>Audio Processing with FFT</a>",
+					"name": "Audio Processing with FFT",
 					"description": "A project to change familiarity with Fourier Transforms. It features usage of the FFT to dissect audio files and display their sampled frequencies in spectrograms and also usage of the IFFT to construct new audio files.",
-					"graphic": "graphics/audioprocessing.png"
+					"graphic": "graphics/audioprocessing.png",
+					"link": "AudioProcessing/"
 				},
 				{
-					"name": "<a href='PatternProcessing/'>Puzzle Pattern Processing</a>",
+					"name": "Puzzle Pattern Processing",
 					"description": "An introductory project to learning OpenCV. Here, several edits were made to a child's pattern matching puzzle, such as isolating the symbols from the grid, identifying different symbols, changing their colors, identifying patterns, and, finally, completing the puzzle by copying the patterns within their rows.",
-					"graphic": "graphics/patternprocessing.png"
+					"graphic": "graphics/patternprocessing.png",
+					"link": "PatternProcessing/"
 				}
 			]
 		},
@@ -59,14 +67,16 @@ const PROJECTS = {
 			"name": "Web Development",
 			"projects": [
 				{
-					"name": "<a href='WebGL/lib/'>WebGL Project</a>",
+					"name": "WebGL Project",
 					"description": "A final project for Iowa State's Introduction to Computer Graphics course. It features the majority of the skills taught in the class using WebGL. Note that the texture designs and dragon model are not my work.",
-					"graphic": "graphics/webgl.mp4"
+					"graphic": "graphics/webgl.mp4",
+					"link": "WebGL/lib/"
 				},
 				{
-					"name": "<a href='snake/'>Snake Game</a>",
+					"name": "Snake Game",
 					"description": "This project is a recreation of the famous snake game. There is not much to it, but it is fun to mess with every now and then.",
-					"graphic": "graphics/snake.png"
+					"graphic": "graphics/snake.png",
+					"link": "snake/"
 				},
 				{
 					"name": "CyBuzz",
@@ -79,9 +89,10 @@ const PROJECTS = {
 					"graphic": "graphics/canvasutility.mp4"
 				},
 				{
-					"name": "<a href='https://youtu.be/cxlqhyEJrZc'>Rudiments Android App Web Server</a>",
+					"name": "Rudiments Android App Web Server",
 					"description": "Rudiments is an Android rhythm game built as a term project for Iowa State's Software Development Practices course. My responsibility within this project was as the sole backend developer. The backend supports level downloads for both native and user-created levels, scoreboards, multiplayer, and administrative controls.",
-					"graphic": "https://www.youtube.com/embed/cxlqhyEJrZc"
+					"graphic": "https://www.youtube.com/embed/cxlqhyEJrZc",
+					"link": "https://youtu.be/cxlqhyEJrZc"
 				}
 			]
 		},
@@ -99,14 +110,16 @@ const PROJECTS = {
 					"graphic": "graphics/pathway.png"
 				},
 				{
-					"name": "<a href='https://kmkilburg28.itch.io/the-ruins-of-sarcopha-gus' target='_blank'>Ruins of SarcophaGus</a>",
+					"name": "Ruins of SarcophaGus",
 					"description": "Ruins of SarcophaGus is a first-person shooter-esque game where the user is a skeleton trapped at the bottom of their tomb and must escape by clearing and ascending each floor. Ruins of SarcophaGus was my first game and was developed at the Iowa State Game Development Club's Fall 2018 Game Jam. This game jam lasted 48 hours and is where our game won an honorable mention.",
-					"graphic": "graphics/ruinsofsarcophagus.png"
+					"graphic": "graphics/ruinsofsarcophagus.png",
+					"link": "https://kmkilburg28.itch.io/the-ruins-of-sarcopha-gus"
 				},
 				{
-					"name": "<a href='https://globalgamejam.org/2019/games/second-chance-0' target='_blank'>2nd Chance</a>",
+					"name": "2nd Chance",
 					"description": "2nd Chance is an isometric 2.5D game where the user plays as a newly formed ghost while fighting away evil spirits and investigating their current predicate. This game was completed as a challenge to Iowa State Game Development Club's Spring 2019 Game Jam with a time limit of 48 hours.",
-					"graphic": "graphics/secondchance.png"
+					"graphic": "graphics/secondchance.png",
+					"link": "https://globalgamejam.org/2019/games/second-chance-0"
 				}
 			]
 		}
@@ -140,7 +153,10 @@ for (let categoryInd = 0; categoryInd < PROJECTS.categories.length; ++categoryIn
 		let text_div = document.createElement('div');
 		text_div.classList.add('col-5');
 		let project_heading = document.createElement('h4');
-		project_heading.innerHTML = project.name;
+		let project_heading_html = project.name
+		if (project['link'])
+			project_heading_html = `<a href='${project.link}'>${project_heading_html}</a>`;
+		project_heading.innerHTML = project_heading_html;
 		text_div.appendChild(project_heading);
 		let project_description = document.createElement('p');
 		project_description.innerHTML = project.description;
